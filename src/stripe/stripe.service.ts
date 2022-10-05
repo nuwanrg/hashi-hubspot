56211f20d60a1c1ceb774ded6b911ff4a380ae7e
nuwanrg <nuwan_rajitha@outlook.com>
2022-10-05 09:53:58 +0530

    added live price id

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -25,7 +25,7 @@ export class StripeService {
             },
             unit_amount: 2000,
           },*/
-          price: 'price_1LnJWaALa3pUXDKn9sy09cnl',
+          price: 'price_1Ll4ggALa3pUXDKn1pzTPPzm', // test mode: price_1LnJWaALa3pUXDKn9sy09cnl
           quantity: 1,
         },
       ],
@@ -33,7 +33,7 @@ export class StripeService {
       cancel_url: 'https://app.hubspot.com/login',
       subscription_data: {
         metadata: {
-          code: req.query.code,
+          code: req.query.code, //code passed from hubspot
           sessionID: req.sessionID,
         },
       },
