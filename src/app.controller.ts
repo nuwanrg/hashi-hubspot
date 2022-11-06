203ffa0f8fe533499cb92e88704ac31ec021c057
nuwanrg <nuwan_rajitha@outlook.com>
2022-11-07 00:16:45 +0530

    add auth module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -45,12 +45,13 @@ export class AppController {
   }
 
   @Get('oauth/token') ///:client_id/:redirect_uri/:response_type/:state
-  async token(@Request() req, @Res() res, @Query() query) {
+  token(@Request() req, @Res() res, @Query() query): string {
     //console.log('zoho req : ', req);
     console.log('zoho query get : ', query);
+    return res.json({ success: true });
   }
   @Post('oauth/token') ///:client_id/:redirect_uri/:response_type/:state
-  async token2(@Request() req, @Res() res, @Query() query): Promise<any> {
+  token2(@Request() req, @Res() res, @Query() query): Promise<any> {
     //console.log('zoho req : ', req);
     console.log('zoho query post: ', query);
     const response = {
@@ -62,6 +63,12 @@ export class AppController {
     };
 
     console.log('zoho query response: ', response);
-    return response;
+    return res.json({
+      access_token:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0ODUxNDA5ODQsImlhdCI6MTQ4NTEzNzM4NCwiaXNzIjoiYWNtZS5jb20iLCJzdWIiOiIyOWFjMGMxOC0wYjRhLTQyY2YtODJmYy0wM2Q1NzAzMThhMWQiLCJhcHBsaWNhdGlvbklkIjoiNzkxMDM3MzQtOTdhYi00ZDFhLWFmMzctZTAwNmQwNWQyOTUyIiwicm9sZXMiOltdfQ.Mp0Pcwsz5VECK11Kf2ZZNF_SMKu5CgBeLN9ZOP04kZo',
+      refresh_token: 'ze9fi6Y9sMSf3yWp3aaO2w7AMav2MFdiMIi2GObrAi-i3248oo0jTQ',
+      token_type: 'Bearer',
+      expires_in: 3600000,
+    });
   }
 }
