ab6c8c35a42f210643e368d01a5dd9f05b198c56
nuwanrg <nuwan_rajitha@outlook.com>
2022-11-09 23:16:21 +0530

    add auth module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -30,39 +30,31 @@ export class AppController {
     return this.oauthService.login(req.user);
   }
 
-  @Get('oauth/auth') ///:client_id/:redirect_uri/:response_type/:state
+  @Get('oauth/auth')
   async auth(@Request() req, @Res() res, @Query() query) {
     //console.log('zoho req : ', req);
     console.log('zoho query : ', query);
     const redirect_uri =
       query.redirect_uri +
-      '?state=' +
-      query.state +
+      // '?state=' +
+      // query.state +
       '&code=pU2DHOWjSCVh6NJKi1ClhBYNKfuqbZVT';
     res.redirect(redirect_uri);
     //return this.oauthService.auth(req);
     //return 'success';
   }
 
-  @Get('oauth/token') ///:client_id/:redirect_uri/:response_type/:state
+  @Get('oauth/token')
   token(@Request() req, @Res() res, @Query() query): string {
     //console.log('zoho req : ', req);
     console.log('zoho query get : ', query);
     return res.json({ success: true });
   }
-  @Post('oauth/token') ///:client_id/:redirect_uri/:response_type/:state
+
+  @Post('oauth/token')
   token2(@Request() req, @Res() res, @Query() query): Promise<any> {
     //console.log('zoho req : ', req);
     console.log('zoho query post: ', query);
-    const response = {
-      access_token:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0ODUxNDA5ODQsImlhdCI6MTQ4NTEzNzM4NCwiaXNzIjoiYWNtZS5jb20iLCJzdWIiOiIyOWFjMGMxOC0wYjRhLTQyY2YtODJmYy0wM2Q1NzAzMThhMWQiLCJhcHBsaWNhdGlvbklkIjoiNzkxMDM3MzQtOTdhYi00ZDFhLWFmMzctZTAwNmQwNWQyOTUyIiwicm9sZXMiOltdfQ.Mp0Pcwsz5VECK11Kf2ZZNF_SMKu5CgBeLN9ZOP04kZo',
-      refresh_token: 'ze9fi6Y9sMSf3yWp3aaO2w7AMav2MFdiMIi2GObrAi-i3248oo0jTQ',
-      token_type: 'Bearer',
-      expires_in: 3600000,
-    };
-
-    console.log('zoho query response: ', response);
     return res.json({
       access_token:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0ODUxNDA5ODQsImlhdCI6MTQ4NTEzNzM4NCwiaXNzIjoiYWNtZS5jb20iLCJzdWIiOiIyOWFjMGMxOC0wYjRhLTQyY2YtODJmYy0wM2Q1NzAzMThhMWQiLCJhcHBsaWNhdGlvbklkIjoiNzkxMDM3MzQtOTdhYi00ZDFhLWFmMzctZTAwNmQwNWQyOTUyIiwicm9sZXMiOltdfQ.Mp0Pcwsz5VECK11Kf2ZZNF_SMKu5CgBeLN9ZOP04kZo',
