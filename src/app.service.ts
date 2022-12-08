3bf92624c07bd7b380299b75b885e83a7b3c2a6d
nuwanrg <nuwan_rajitha@outlook.com>
2022-12-08 15:38:03 +0530

    add logs

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,21 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const res = `{
-      "objectId": 988,
-      "title": "API-54: Question about bulk APIs",
-      "link": "http://example.com/2",
-      "created": "2016-08-04",
-      "priority": "HIGH",
-      "project": "API",
-      "reported_by": "ksmith@hubspot.com",
-      "description": "Customer is not able to find documentation about our bulk Contacts APIs.",
-      "reporter_type": "Support Rep",
-      "status": "Resolved",
-      "ticket_type": "Bug",
-      "updated": "2016-09-23"
-     }`;
-
-    return JSON.stringify(res);
+    return 'hashi-hubspot-running';
   }
 }
