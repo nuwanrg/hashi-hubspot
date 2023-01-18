34072e307e2447bf13c0c72d378b932881258877
nuwanrg <nuwan_rajitha@outlook.com>
2023-01-18 12:15:53 +0530

    get user account info sync

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { UsersModule } from 'src/users/users.module';
 import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, HttpModule],
   providers: [UsersService],
 })
 export class AuthModule {}
